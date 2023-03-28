f1361fda95f55b89fe6ea1bd93de1d2dcc860451
amoguszzz <104812506+amoguszzz@users.noreply.github.com>
2023-03-28 13:48:48 -0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -14,5 +14,8 @@ tab1:CreateSlider("SLIDER", 1,100, function(value) --slider
     print(value)
 end)
 tab1:CreateColorPicker("XD", Color3.fromRGB(255,255,255), function(v) --color picker
-    print(v)
+    main:changeColor1(v) --set UILIB accent color
+end)
+tab1:CreateKeyBind("KeyBind", Enum.KeyCode.F1, function() -- keybind
+	main:UIToggle() --make ui lib inivislble
 end)
